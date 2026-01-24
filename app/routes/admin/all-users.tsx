73b78a61fbef0b2d19d5b00818bc7e9a5885224d
Rafli Alif Ihza Hartono <64510812+rhaffle87@users.noreply.github.com>
2026-01-24 12:17:42 +0700

    routing

---
+++ app/routes/admin/all-users.tsx
@@ -0,0 +1,7 @@
+const allUsers = () => {
+  return (
+    <div>Users Table</div>
+  )
+}
+
+export default allUsers
---
+++ app/routes/admin/dashboard.tsx
@@ -0,0 +1,7 @@
+const dashboard = () => {
+  return (
+    <div>dashboard Page</div>
+  )
+}
+
+export default dashboard
